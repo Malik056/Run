26f6b5a8bc785e781336050b66b960bed96bb9d6
TahaMalik <tahamahmoodmalik@gmail.com>
2019-05-09 23:35:30 +0500

    initialized with errors

--- app/src/main/java/com/example/mobeen/run/MapsActivity.java
+++ app/src/main/java/com/example/mobeen/run/MapsActivity.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.mobeen.run.Models.Venue;
-//import com.google.android.gms.maps.CameraUpdateFactory;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import com.google.android.gms.maps.SupportMapFragment;
-//import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import java.util.List;
 
@@ -20,22 +21,22 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MapsActivity extends FragmentActivity{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
         //implements OnMapReadyCallback
 
-//    private GoogleMap mMap;
-    //private Context context;
+    private GoogleMap mMap;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-//        context = this;
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
+        context = this;
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
 
@@ -48,34 +49,34 @@ public class MapsActivity extends FragmentActivity{
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//
-//        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-//        Call<List<Venue>> call = service.getAllVenues();
-//        call.enqueue(new Callback<List<Venue>>() {
-//            @Override
-//            public void onResponse(Call<List<Venue>> call, Response<List<Venue>> response) {
-//                for(int i=0;i<response.body().size();i++){
-//
-//                    LatLng venue1 = new LatLng(Double.parseDouble(response.body().get(i).getLatitude()),Double.parseDouble(response.body().get(i).getLongitude()));
-//                    mMap.addMarker(new MarkerOptions().position(venue1).title(response.body().get(i).getName()).icon(BitmapDescriptorFactory.fromResource(R.drawable.stadium_icon)).snippet("venue"));
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Venue>> call, Throwable t) {
-//                Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//
-//        LatLng base = new LatLng(31.4812031, 74.3009524);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(base));
-//        mMap.setMinZoomPreference(12.0f);
-//
-//    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<List<Venue>> call = service.getAllVenues();
+        call.enqueue(new Callback<List<Venue>>() {
+            @Override
+            public void onResponse(Call<List<Venue>> call, Response<List<Venue>> response) {
+                for(int i=0;i<response.body().size();i++){
+
+                    LatLng venue1 = new LatLng(Double.parseDouble(response.body().get(i).getLatitude()),Double.parseDouble(response.body().get(i).getLongitude()));
+                    mMap.addMarker(new MarkerOptions().position(venue1).title(response.body().get(i).getName()).icon(BitmapDescriptorFactory.fromResource(R.drawable.stadium_icon)).snippet("venue"));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Venue>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        LatLng base = new LatLng(31.4812031, 74.3009524);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(base));
+        mMap.setMinZoomPreference(12.0f);
+
+    }
 
 
 }
