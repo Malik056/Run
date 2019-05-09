4ac4f34f7a77ed99be3a5e0916b7ae5a1e6797c7
TahaMalik <tahamahmoodmalik@gmail.com>
2019-05-09 20:46:55 +0500

    initialized with errors

--- app/src/main/java/com/example/mobeen/run/PhoneAuthActivity.java
+++ app/src/main/java/com/example/mobeen/run/PhoneAuthActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -83,6 +84,7 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
         @Override
         public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getApplicationContext(), "Failed Authentication", Toast.LENGTH_LONG).show();
         }
 
         @Override
@@ -90,6 +92,8 @@ public class PhoneAuthActivity extends AppCompatActivity {
 
             mVerificationId = verificationId;
             mResendToken = token;
+            Toast.makeText(getApplicationContext(), "Code Sent", Toast.LENGTH_LONG).show();
+
         }
     };
 
